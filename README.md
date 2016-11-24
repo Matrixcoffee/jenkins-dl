07f90b578ebd57a59ea162e2ada2e86a83e606a5
Matrixcoffee <Matrixcoffee@users.noreply.github.com>
2016-11-24 00:15:05 +0000

    Actual initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # jenkins-dl
 Downloader for Jenkins build artifacts.
+
+A simple script for downloading Jenkins build artifacts. It is preconfigured
+to download the F-Droid build of Riot Android.
+
+## Features
+* Give the file a useful name with build number and commit hash
+
+## Configuration
+Edit the variables in the script.
+
+Set `FLAVOUR` to `vector-app-debug` to download the Play Store
+version.
+
+## Dependencies
+* awk
+* wget
---
+++ jenkins-dl.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+fail () {
+	echo "Failed $*."
+	exit 1
+}
+
+BUILDNO="$1"
+
+FLAVOUR=vector-appfdroid-debug
+
+FILE="$FLAVOUR.apk"
+BUILDPATH="https://matrix.org/jenkins/job/VectorAndroidDevelop/$BUILDNO"
+BUILDPATH2="$BUILDPATH/"
+FILEPATH="artifact/vector/build/outputs/apk/$FILE"
+FPRPATH="$BUILDPATH/$FILEPATH/*fingerprint*/"
+
+EVALCODE="$( wget -O - "$BUILDPATH2" "$FPRPATH" | awk '
+	BEGIN {
+		revision="unknown"
+		fingerprint="unknown"
+	}
+
+	/<b>Revision<\/b>:/ {
+		sub("^.*<b>Revision</b>:[ \t]+", ""); l1=length($0);
+		gsub("[^A-Za-z0-9]", ""); l2=length($0);
+		if (l1 == 40 && l2 == 40) {
+			revision=$0
+		}
+	}
+
+	/<p>The fingerprint [A-Za-z0-9]+ did not match any of the recorded data.<\/p>/ {
+		sub("^.*fingerprint[ \t]+", "");
+		sub("[ \t].*", ""); l1=length($0);
+		gsub("[^A-Za-z0-9]", ""); l2=length($0);
+		if (l1 == 32 && l2 == 32) {
+			fingerprint=$0
+		}
+	}
+
+	END {
+		print "REVISION=" tolower(revision)
+		print "FINGERPRINT=" tolower(fingerprint)
+	}' )"
+
+#echo "Eval: \"$EVALCODE\""
+
+[ -n "$EVALCODE" ] || fail retrieving metadata
+
+eval $EVALCODE
+
+DESTFILE="$FLAVOUR-$BUILDNO-$REVISION".apk
+
+echo
+echo "Build: $BUILDNO"
+echo "Flavour: $FLAVOUR"
+echo "Revision: $REVISION"
+echo "Fingerprint: $FINGERPRINT"
+echo "Target: $DESTFILE"
+echo
+
+if [ -e "$DESTFILE" ]; then
+	if echo "$FINGERPRINT  $DESTFILE" | md5sum -c; then
+		exit 0
+	else
+		rm -v "$DESTFILE"
+	fi
+fi
+
+[ ! -e "$DESTFILE" ] || fail deleting target file
+[ ! -e "$DESTFILE" ] && wget -t 1 "$BUILDPATH/$FILEPATH" -O "$DESTFILE"
+[ -e "$DESTFILE" ] || fail downloading file
+if echo "$FINGERPRINT  $DESTFILE" | md5sum -c; then
+	exit 0
+else
+	rm -v "$DESTFILE"
+	fail downloading file: Bad checksum
+fi
